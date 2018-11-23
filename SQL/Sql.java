90ef5233da71a151d99856aad5fe660f352ab0f6
Adam Jones <ajones20@sheffield.ac.uk>
2018-11-23 11:48:31 +0000

    Add files via upload

--- SQL/Sql.java
+++ SQL/Sql.java
@@ -9,6 +9,7 @@ import java.sql.SQLException;
 import classPkg.UserInfo;
 
 public class Sql {
+	// find user
 	public UserInfo checkLogIn(String usr, String pw) throws SQLException {
 		Connection con = setUpConnection();
 		int ID = 0;
@@ -35,6 +36,44 @@ public class Sql {
 			return null;
 	}
 
+	// find/show fns still to do
+	// find student info from regNo, create studentInfo class
+	// find modules from posRegNo, returns list of module classes (need to create)
+	// find PoSG, return Grade as double (or make PoS class)
+	// show student progress (return all PoS's)
+
+	// add fns
+	// add new User
+	// add new department
+	// add course
+	// add module
+	// add student
+	// add taken module
+	// add a grade for module
+	// add resit grade for module
+
+	// remove fns
+	// delete user
+	// delete department
+	// delete course
+	// delete module
+	// delete student
+	// drop taken module
+
+	// update fns
+	// update awarded class
+	// update grade (ask for grade type in param, norm or resit)
+
+	// other fns
+	// check taken module are applicable (right level of study and module)
+	// check taken module credit total (could leave for josh and tom)
+	// calc weighted mean grade (could leave for tom and josh as no sql involved
+	// other than getting all PoS grades)
+	// pass or fail (same as above, we just need a fn to return PoSmodulesinfo like
+	// find module)
+	// find awarded class in final year (as above)
+
+	// get connection
 	public Connection setUpConnection() throws SQLException {
 		Connection con = null;
 		try {
