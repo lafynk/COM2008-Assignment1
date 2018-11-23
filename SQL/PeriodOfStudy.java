ba7c79dd33b888e06d4da96770206715bdb22af7
Adam Jones <ajones20@sheffield.ac.uk>
2018-11-23 16:46:34 +0000

    Add files via upload

---
+++ SQL/PeriodOfStudy.java
@@ -0,0 +1,51 @@
+package classPkg;
+
+import java.util.Date;
+
+public class PeriodOfStudy {
+	private int RegNo;
+	private char PoS;
+	private Date startDate;
+	private Date endDate;
+	private char level;
+	private double grade;
+	private boolean progress;
+
+	public PeriodOfStudy(int r, char p, Date s, Date e, char l, double g, boolean pr) {
+		RegNo = r;
+		PoS = p;
+		startDate = s;
+		endDate = e;
+		level = l;
+		grade = g;
+		progress = pr;
+	}
+
+	public int getRegNo() {
+		return RegNo;
+	}
+
+	public char getPoS() {
+		return PoS;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public char getLevel() {
+		return level;
+	}
+
+	public double getGrade() {
+		return grade;
+	}
+
+	public boolean getProgress() {
+		return progress;
+	}
+}
