718694d1d3143cf1396db4903799bad54ea039cf
Josh Barrows <34284924+bamajo@users.noreply.github.com>
2018-11-24 19:51:03 +0000

    added registrar student add/delete

--- GUI/registrarpage.java
+++ GUI/registrarpage.java
@@ -1,7 +1,12 @@
 package GUI;
 import java.awt.*;
 import javax.swing.*;
+import java.util.Random;
+import SQLcode.Sql;
+import classPkg.UserInfo;
+
 import java.awt.event.ActionListener;
+import java.sql.SQLException;
 import java.awt.event.ActionEvent;
 
 
@@ -12,6 +17,7 @@ public class registrarpage extends JFrame {
 		 }
 	 
 public registrarpage() throws HeadlessException {
+	Sql s = new Sql();
 	JFrame window = new JFrame("Systems Design & Security: Team Project");
 	 window.setLayout(null);
 	 window.setSize(1000,1000);
@@ -24,18 +30,38 @@ public registrarpage() throws HeadlessException {
 	 adminlabel.setFont(new Font("", Font.PLAIN, 30));
 	 window.add(adminlabel);
 	 
-	 JTextField studentaccount = new JTextField("Type student to add/delete here");
+	 JTextField studentaccount = new JTextField("Type student ID to delete here");
 	 studentaccount.setBounds(10,100,250,50);
 	 window.add(studentaccount);
 	 
-     JTextField studentdegree = new JTextField("Type student degree (if creating acct)");
-     studentdegree.setBounds(260,100,250,50);
+     JTextField studentdegree = new JTextField("Type student degree code (if creating acct)");
+     studentdegree.setBounds(10,150,250,50);
 	 window.add(studentdegree);
+	 
+	 JTextField studentfname = new JTextField("Type student forename (if creating acct)");
+     studentfname.setBounds(10,200,250,50);
+	 window.add(studentfname);
+	 
+	 JTextField studentsname = new JTextField("Type student surname (if creating acct)");
+     studentsname.setBounds(10,250,250,50);
+	 window.add(studentsname);
+	 
+	 JTextField studentemail = new JTextField("Type student email (if creating acct)");
+     studentemail.setBounds(10,300,250,50);
+	 window.add(studentemail);
+	 
+	 JTextField studenttitle = new JTextField("Type student title (if creating acct)");
+     studenttitle.setBounds(360,250,250,50);
+	 window.add(studenttitle);
+	 
+	 JTextField studenttutor = new JTextField("Type student tutor (if creating acct)");
+     studenttutor.setBounds(360,200,250,50);
+	 window.add(studenttutor);
 	 
 	 JComboBox<String> lvl = new JComboBox<String>();
 	 
 	// add items to the combo box
-	 lvl.addItem("year 1");
+	 lvl.addItem("1");
 	 lvl.addItem("2");
 	 lvl.addItem("3");
 	 lvl.addItem("4");
@@ -53,6 +79,15 @@ public registrarpage() throws HeadlessException {
     public void actionPerformed(ActionEvent e) {
         //your actions
         // delte acct function call
+    	try {
+			s.removeStudent(Integer.parseInt(studentaccount.getText()));
+		} catch (NumberFormatException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
     }
     });
      	 JButton addstudentbutton = new JButton("Add Student");
@@ -64,21 +99,30 @@ public registrarpage() throws HeadlessException {
     public void actionPerformed(ActionEvent e) {
         //your actions
         // add acct function call
+    	//UserInfo u=null;
+    	String makeemail =( studentfname.getText().substring(0, 1)) + studentsname.getText() + "2"+"@sheffield.ac.uk";
+		try {
+			Random rand = new Random();
+			s.addStudent(100000000 + rand.nextInt(900000000), studenttitle.getText(), studentsname.getText(),studentfname.getText(), makeemail, studenttutor.getText(), studentdegree.getText(), lvl.getSelectedItem().toString().charAt(0), "");
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
     }
     });
      
      /////////////////////////////////
      
-	 JTextField module = new JTextField("Type module to add/drop here");
-	 module.setBounds(10,200,300,50);
+	 JTextField module = new JTextField("Type module code to add/drop here");
+	 module.setBounds(10,400,300,50);
 	 window.add(module);
-	 JTextField modulefordegree = new JTextField("Type student to assign module to");
-	 modulefordegree.setBounds(10,250,300,50);
+	 JTextField modulefordegree = new JTextField("Type student ID to assign module to");
+	 modulefordegree.setBounds(10,450,300,50);
 	 window.add(modulefordegree);
 	 
 	 
 	 JButton addmodulebutton = new JButton("Add module");
-	 addmodulebutton.setBounds(350,200,200,50);
+	 addmodulebutton.setBounds(350,400,200,50);
 	 window.add(addmodulebutton);
 	 addmodulebutton.addActionListener(new ActionListener() {
 
@@ -89,7 +133,7 @@ public registrarpage() throws HeadlessException {
     }
     });
      	 JButton deletemodulebutton = new JButton("Drop module");
-	 deletemodulebutton.setBounds(600,200,200,50);
+	 deletemodulebutton.setBounds(600,400,200,50);
 	 window.add(deletemodulebutton);
 	 deletemodulebutton.addActionListener(new ActionListener() {
 
@@ -108,7 +152,7 @@ yearofstudy.addItem("2");
 yearofstudy.addItem("3");
 yearofstudy.addItem("4");
 yearofstudy.addItem("5");
-yearofstudy.setBounds(350,260,100,50);
+yearofstudy.setBounds(350,460,100,50);
 window.add(yearofstudy);
      
 JComboBox<String> period = new JComboBox<String>();
@@ -118,7 +162,7 @@ period.addItem("autumn semester");
 period.addItem("spring semester");
 period.addItem("all year");
 
-period.setBounds(460,260,140,50);
+period.setBounds(460,460,140,50);
 window.add(period);
   
 
