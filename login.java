05dd025a2ed452649db498ad383aa63f27f7af86
Josh Barrows <34284924+bamajo@users.noreply.github.com>
2018-11-24 17:36:56 +0000

    Update login.java
    Login corrections.

--- login.java
+++ login.java
@@ -9,7 +9,7 @@ public class login extends JFrame {
 		 // launching code goes in here
 			 new login();
 		 }
-	 
+
 public login() throws HeadlessException {
 	JFrame window = new JFrame("Systems Design & Security: Team Project");
 	 window.setLayout(new FlowLayout());
@@ -17,16 +17,16 @@ public login() throws HeadlessException {
 	 window.setVisible(true);
 	 window.setResizable(false);
 	 window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	 
 
 
-	 
+
+
 	 JTextField username = new JTextField();
 	 username.setBounds(400,400,200,50);
 	 window.add(username);
-	 
-	
-	 
+
+
+
 	 JButton loginbutton = new JButton("Login");
 	 loginbutton.setBounds(400,600,200,50);
 	 window.add(loginbutton);
@@ -36,25 +36,26 @@ public login() throws HeadlessException {
     public void actionPerformed(ActionEvent e) {
         //your actions
         //window.exit();
-        window.dispose();
-        new studentpage();
+      //  window.dispose();
+      //  new studentpage();
+			JOptionPane.showMessageDialog(null, "Username or password is incorrect","Login Failure!", JOptionPane.ERROR_MESSAGE);
     }
     });
 
-	 JLabel usernamelabel = new JLabel("USerame");
+	 JLabel usernamelabel = new JLabel("Username");
 	 usernamelabel.setBounds(400,340,200,50);
 	 window.add(usernamelabel);
-	
-	 JLabel pwdlabel = new JLabel("PAssrd");
+
+	 JLabel pwdlabel = new JLabel("Password");
 	 pwdlabel.setBounds(400,440,200,50);
 	 window.add(pwdlabel);
 
 	 JTextField password = new JTextField();
 	 password.setBounds(400,500,200,50);
 	 window.add(password);
-	 
+
 	 window.repaint();
-		 
+
  }
 
 }
